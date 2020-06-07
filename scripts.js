21aa50b31c92586850bfbcb8f79df1c2fbebf1f0
Matyas Toth <65894302+BobaFett7790@users.noreply.github.com>
2020-06-07 21:19:33 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -10,3 +10,26 @@ console.log(myArray[0]);
 let colors = ['Black', 'Red', 'Blue'];
 
 console.log(colors[1])
+
+let bookCount = 10;
+for (let i = 0; i < bookCount; i++)
+
+console.log(i++)
+
+let add = (a, b) => {
+    return a + b;
+}
+
+console.log(add(1,4))
+
+// for (let step = 0; step < 1; step++) {
+//     console.log('Walking east one step');
+//   }
+
+let str = '';
+
+for (let i = 0; i < 9; i++) {
+  str = str + i;
+}
+
+console.log(str);
