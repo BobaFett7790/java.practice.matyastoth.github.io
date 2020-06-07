9d808b8d529a9fc14417b52a0bd7c43e4c813926
Matyas Toth <65894302+BobaFett7790@users.noreply.github.com>
2020-06-07 20:32:00 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,3 +2,11 @@ let age = 30
 age
 
 console.log("JavaScript os working!")
+
+let myArray = ['Judit', 'Matyi',];
+
+console.log(myArray[0]);
+
+let colors = ['Black', 'Red', 'Blue'];
+
+console.log(colors[1])
